772962336102b86382348e432e020b23a40046c2
Dairojhon <112525272+Dairojhon@users.noreply.github.com>
2022-08-30 19:57:29 -0500

    actualización

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         var resultado= suma(25,67,35);
-        System.out.println(resultado);
+        System.out.println("El resultado de la suma es: "+resultado);
         coche miCoche = new coche();
         miCoche.Incpuertas();
-        System.out.println(miCoche.puertas);
+        System.out.println("El número de puertas del coche es: "+miCoche.puertas);
     }
     public static int suma(int a, int b, int c)
     {
